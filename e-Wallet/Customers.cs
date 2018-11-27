5040b77dfa5aae8a3e22b4612af4ead63a9aa679
Ram <ramprm@gmail.com>
2018-11-27 17:35:51 +0800

    Add project files.

---
+++ e-Wallet/Customers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project5
+{
+    public class Customers
+    {
+        public Customer[] data { get; set; }
+        public Collection collection { get; set; }
+    }
+
+    public class Collection
+    {
+        public int current_page { get; set; }
+        public int per_page { get; set; }
+        public int total_entries { get; set; }
+        public int total_pages { get; set; }
+    }
+
+    public class Customer
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public object additional_first_name { get; set; }
+        public object title { get; set; }
+    }
+}
+
---
+++ e-Wallet/callback.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FintechAPI;
+
+namespace Project5
+{
+    public partial class callback : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Instantiate an OAuth2 object
+            OAuth2 oAuth2 = new OAuth2();
+
+            //Configure the parameters and callback URL
+            oAuth2.client_id = "f01805e4766ebdc5";
+            oAuth2.client_secret = "be744c6869ffac80f421742f9aff0732";
+            oAuth2.redirect_uri = "http://localhost:3000/callback.aspx";
+
+            //request if OAuth2 server has granted a code
+            string code = Request.QueryString["code"];
+            
+            //Instantiate a status object
+            APIAccess.Result status = new APIAccess.Result();
+            status.result = -1;
+            status.value = "";
+
+            //Instantiate a APIAccess object
+            APIAccess fidorapi = new APIAccess();
+
+            //Status returned from GetAccessToken API
+            status = fidorapi.GetAccessToken(oAuth2, code);
+
+            if (status.result == 0)
+            {
+                //save the access token
+                Session["AccessToken"] = status.value;
+                //go to a main page
+                Response.Redirect("Index.aspx");
+            }            
+        }
+    }
+}
